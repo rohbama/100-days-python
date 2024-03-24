5b8dca441f9116b1637ed391bc80723c09b05118
rohbama <rohansm2008@gmail.com>
2024-03-24 14:07:01 +0530

    done with day 1 project

---
+++ Day-001/project.py
@@ -0,0 +1,8 @@
+# band name generator
+
+print("this is a band name suggestion")
+
+city = input('which city did you grow up in?\n')
+pet_name = input("what is the name of your pet\n")
+
+print("this is your band name suggestion " + city + " " + pet_name + "s")
